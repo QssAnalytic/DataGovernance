4a6d2b2a8965bf0f22fc948b1f599d6efe0d363d
Farid Sattarov <feridsattarov5@gmail.com>
2024-12-04 12:16:59 +0400

    changing content headerPart

--- src/components/Shared/header/HeaderProfileSide.tsx
+++ src/components/Shared/header/HeaderProfileSide.tsx
@@ -1,42 +1,21 @@
-import { useUIStore } from "../../../services/store/useUIStore";
-import { useAdminStore } from "@/services/store/useAdminStore";
-import { Plus } from "lucide-react";
+import { FiBell } from "react-icons/fi";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export const ChangingContent = () => {
-    const { buttonLabel, isModalOpen, setIsModalOpen, pageCustomHeader, modalContent } = useUIStore();
-    const { isAdmin } = useAdminStore();
+export const HeaderProfileSide = () => {
+  const notifications: string[] = ["Notification 1", "Notification 2"]; // Example notifications
 
-    return (
-        <div className="flex gap-x-[20px] items-center">
-            {/* Render a dynamic header */}
-            {pageCustomHeader}
-
-            {/* Show the button only for admin users */}
-            {isAdmin && (
-                <button
-                    className="px-[16px] min-w-fit py-3 flex items-center gap-x-[12px] rounded-xl bg-[#22385F] text-sm font-medium text-white"
-                    onClick={() => { setIsModalOpen(!isModalOpen); }} // Toggle modal visibility
-                >
-                    <Plus />
-                    {buttonLabel} {/* Dynamic label for the button */}
-                </button>
-            )}
-
-            {/* Modal component appears when isModalOpen is true */}
-            {isModalOpen && (
-                <div className="z-[100px] w-full top-0 left-0 h-[100vh] flex items-center justify-center absolute">
-                    {/* Overlay for closing the modal */}
-                    <div 
-                        className="overlay absolute z-40 bg-black bg-opacity-50 w-full h-[100vh]" 
-                        onClick={() => setIsModalOpen(false)} // Close modal when overlay is clicked
-                    ></div>
-
-                    {/* Modal content box */}
-                    <div className="flex bg-white flex-col min-w-[200px] min-h-[200px] z-50">
-                        {modalContent} {/* Dynamic content rendered inside the modal */}
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div className="flex gap-x-[20px] items-center relative">
+      <div className="relative">
+        <FiBell className="text-[rgba(34,56,95,1)] w-[32px] h-[32px]" />
+        {notifications.length > 0 && (
+          <span className="absolute top-[-3px] right-[3px] w-[12px] h-[12px] bg-[#E02222] rounded-full border border-[white]" />
+        )}
+      </div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
 };
