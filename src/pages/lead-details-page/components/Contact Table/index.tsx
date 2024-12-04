1bcccc5cf26bee1c6febdbff2244f875e3de09bd
Khalida <xalide.haqverdiyeva@gmail.com>
2024-12-04 10:36:22 +0400

    bug-fix: use state added

--- src/pages/lead-details-page/components/Contact Table/index.tsx
+++ src/pages/lead-details-page/components/Contact Table/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
+import React, { useState } from "react";
 import { GrRefresh } from "react-icons/gr";
 
 import {
