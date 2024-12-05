225fd58f4cca3eedb82231e30b3c0f7c787a853b
Huseyn Emin <huseynliemin68@gmail.com>
2024-12-05 10:41:12 +0000

    Emin initial push

---
+++ src/pages/tracker/components/Navbar.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { ParticipantList } from "./ParticipantList";
+import { TrainerHours } from "./TrainerHours";
+import { AttendanceSheet } from "./AttendanceSheet";
+import { ExecutiveReport } from "./ExecutiveReport";
+import { ParticipantSatisfaction } from "./ParticipantSatisfaction";
+
+export const Navbar = () => {
+  const [activeComponent, setActiveComponent] = useState("ParticipantList");
+
+  const renderActiveComponent = () => {
+    switch (activeComponent) {
+      case "ParticipantList":
+        return <ParticipantList />;
+      case "TrainerHours":
+        return <TrainerHours />;
+      case "AttendanceSheet":
+        return <AttendanceSheet />;
+      case "ExecutiveReport":
+        return <ExecutiveReport />;
+      case "ParticipantSatisfaction":
+        return <ParticipantSatisfaction />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div>
+      <ul className="flex items-center h-[45px] gap-[20px] text-[#BCBCBC] border-b-[1px] border-[#ECECEC] p-[0px_379px_1px_40px]">
+        <li
+          className={`text-center font-[Montserrat] text-[16px] not-italic font-semibold leading-normal cursor-pointer ${
+            activeComponent === "ParticipantList" ? "text-black" : ""
+          }`}
+          onClick={() => setActiveComponent("ParticipantList")}
+        >
+          Participant list
+        </li>
+        <li
+          className={`text-center text-[16px] not-italic font-semibold leading-normal cursor-pointer ${
+            activeComponent === "TrainerHours" ? "text-black" : ""
+          }`}
+          onClick={() => setActiveComponent("TrainerHours")}
+        >
+          Trainer hours
+        </li>
+        <li
+          className={`text-center text-[16px] not-italic font-semibold leading-normal cursor-pointer ${
+            activeComponent === "AttendanceSheet" ? "text-black" : ""
+          }`}
+          onClick={() => setActiveComponent("AttendanceSheet")}
+        >
+          Attendance sheet
+        </li>
+        <li
+          className={`text-center text-[16px] not-italic font-semibold leading-normal cursor-pointer ${
+            activeComponent === "ExecutiveReport" ? "text-black" : ""
+          }`}
+          onClick={() => setActiveComponent("ExecutiveReport")}
+        >
+          Executive report
+        </li>
+        <li
+          className={`text-center text-[16px] not-italic font-semibold leading-normal cursor-pointer ${
+            activeComponent === "ParticipantSatisfaction" ? "text-black" : ""
+          }`}
+          onClick={() => setActiveComponent("ParticipantSatisfaction")}
+        >
+          BC-Participant overall satisfaction
+        </li>
+      </ul>
+
+      <div className="mt-4">{renderActiveComponent()}</div>
+    </div>
+  );
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -6,12 +6,14 @@ import {
 import Layout from "./layout";
 import Home from "./pages/home";
 import DetailsPage from "./pages/lead-details-page";
+import Tracker from "./pages/tracker";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="t" element={<Tracker/>} />
         <Route path="/details" element={<DetailsPage />} />
       </Route>
     </Route>
