62014e740ce5b2cf235591e50968d24ab4ef76d9
Khalida <xalide.haqverdiyeva@gmail.com>
2024-12-06 12:01:15 +0400

    feat: date picker buttons added

---
+++ src/pages/lead-details-page/components/Custom Calendar/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import * as React from "react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function DatePickerDemo({ placeholder }: { placeholder: string }) {
+  const [date, setDate] = React.useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "focus:outline-none w-[100%] focus:ring-0 border py-[12px] px-[16px] placeholder:text-[14px] placeholder:text-[#969696] rounded-[12px]  justify-between text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+          style={{ height: "auto", lineHeight: "1.7" }} // Ensure consistent height/line-height
+        >
+          <span className="text-[#969696]">
+            {date ? format(date, "PPP") : placeholder}
+          </span>
+          <CalendarIcon className="ml-2 h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0 rounded-[12px]">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
--- src/pages/lead-details-page/components/Edit Modal/Contact Details Form/index.tsx
+++ src/pages/lead-details-page/components/Edit Modal/Contact Details Form/index.tsx
@@ -1,13 +1,42 @@
+import { DatePickerDemo } from "../../Custom Calendar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../Custom Selector";
+
 const ContactDetailsForm = () => {
   return (
     <div>
-      <form>
-        <input
-          type="text"
-          id="name"
-          className="focus:outline-none focus:ring-0  border py-[12px] placeholder:text-[14px] placeholder:text-[#969696] px-[16px] rounded-[12px]"
-          placeholder="Contact Number"
-        />
+      <form className="flex gap-[20px]">
+        <div className="flex flex-col gap-[10px] flex-1">
+          <input
+            type="text"
+            id="name"
+            className="focus:outline-none focus:ring-0  border py-[12px] placeholder:text-[14px] placeholder:text-[#969696] px-[16px] rounded-[12px]"
+            placeholder="Contact Number"
+          />
+
+          <DatePickerDemo placeholder="Last Contact Date" />
+          <Select>
+            <SelectTrigger className="w-[100%]">
+              <SelectValue placeholder="Value" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="2.5">2.5</SelectItem>
+              <SelectItem value="3">3</SelectItem>
+              <SelectItem value="3.5">3.5</SelectItem>
+              <SelectItem value="4">4</SelectItem>
+              <SelectItem value="5">5</SelectItem>
+              <SelectItem value="6">6</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex-1">
+          <DatePickerDemo placeholder="When to Call Again" />
+        </div>
       </form>
     </div>
   );
