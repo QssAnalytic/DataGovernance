cd2c2da79863bdda4d24f694a36f4fb65555aef6
Khalida <xalide.haqverdiyeva@gmail.com>
2024-12-12 17:16:52 +0400

    bug-fix: remove unused files

--- src/pages/home/DateRangePicker.tsx
+++ src/pages/home/DateRangePicker.tsx
@@ -1,16 +1,16 @@
-import * as React from "react"
-import { addDays, format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
-import { DateRange } from "react-day-picker"
+import * as React from "react";
+import { addDays, format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 
 export function DatePickerWithRange({
   className,
@@ -18,12 +18,11 @@ export function DatePickerWithRange({
   const [date, setDate] = React.useState<DateRange | undefined>({
     from: new Date(2022, 0, 20),
     to: addDays(new Date(2022, 0, 20), 5),
-  })
-  console.log("first")
+  });
   return (
     <div className={cn("grid gap-2", className)}>
       <Popover>
-        <PopoverTrigger asChild className=' '>
+        <PopoverTrigger asChild className=" ">
           <Button
             id="date"
             variant={"outline"}
@@ -32,10 +31,10 @@ export function DatePickerWithRange({
               !date && "text-muted-foreground"
             )}
           >
-            <CalendarIcon className="text-[#22385F]"/>
+            <CalendarIcon className="text-[#22385F]" />
             {date?.from ? (
               date.to ? (
-                <div className='text-black'>
+                <div className="text-black">
                   {format(date.from, "LLL dd, y")} -{" "}
                   {format(date.to, "LLL dd, y")}
                 </div>
@@ -59,5 +58,5 @@ export function DatePickerWithRange({
         </PopoverContent>
       </Popover>
     </div>
-  )
+  );
 }
