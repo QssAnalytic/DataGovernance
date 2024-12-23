792299c216fc8e5879175c68d2f20b86d7105983
tahminarasulzadeh <92018505+tahminarasulzadeh@users.noreply.github.com>
2024-12-23 09:57:18 +0400

    adding changes

---
+++ src/components/Shared/newLeadModal/index.tsx
@@ -0,0 +1,7 @@
+ const NewLeadModal = () => {
+    return <h1 className="text-red-800">
+        NewLeadModal
+    </h1>
+ }
+
+ export default NewLeadModal;
--- src/pages/app-data/components/table/index.tsx
+++ src/pages/app-data/components/table/index.tsx
@@ -8,7 +8,7 @@ import { SharedData } from '../sharedData';
 import { TableRow, TableProps } from '../../types';
 import PaginationControls from '@/pages/lead-details-page/components/Pagination Controller';
 import { HiOutlineChevronUpDown } from "react-icons/hi2";
-import SaveModal from '../SaveModal';
+import SaveModal from '../saveModal';
 
 
 const Table: React.FC<TableProps> = ({ searchTerm }) => {
@@ -52,7 +52,7 @@ const Table: React.FC<TableProps> = ({ searchTerm }) => {
 
         setSortOrder('')
         setSortOrderBy('date');
-        setDateSortOrder(dateSortOrder === 'asc' ? 'desc' : dateSortOrder === 'desc' ? '' : 'asc' );
+        setDateSortOrder(dateSortOrder === 'asc' ? 'desc' : dateSortOrder === 'desc' ? '' : 'asc');
 
     };
 
@@ -110,7 +110,7 @@ const Table: React.FC<TableProps> = ({ searchTerm }) => {
             if (isNaN(dateA)) return 1;  // Place invalid dates at the end
             if (isNaN(dateB)) return -1;
 
-            return dateSortOrder === 'asc' ?  dateA - dateB : dateSortOrder === 'desc' ? dateB - dateA: 0
+            return dateSortOrder === 'asc' ? dateA - dateB : dateSortOrder === 'desc' ? dateB - dateA : 0
         }
         return 0;
     });
@@ -154,51 +154,50 @@ const Table: React.FC<TableProps> = ({ searchTerm }) => {
         <div>
             <div className='max-w-full flex overflow-y-auto max-h-[650px]'>
                 <table className="min-w-[1300px] table-auto w-full rounded-lg border-collapse border border-gray-200">
-                    <thead className="bg-[#FAFAFA]  sticky top-0 z-10">
-                        <tr className="text-left">
+                    <thead className="bg-[#FAFAFA] sticky top-0 z-15 shadow-md">
+                        <tr className="text-left ">
                             <th className="p-3 text-sm font-semibold text-gray-600">ID</th>
                             <th className="border-none text-center px-3 py-2 font-montserrat text-[22px] font-bold leading-normal text-[#000000]">
-                                <div className="flex justify-center">
-                                    <HiOutlinePhone className="w-[24px] h-[24px]" />
+                                <div className="flex  justify-center">
+                                    <HiOutlinePhone className="w-[24px]  h-[24px]" />
                                 </div>
                             </th>
-                            <th >
+                            <th className='py-2 ' >
                                 <div className="flex gap-1">
                                     <HiOutlineChevronUpDown onClick={handleSort} className={`cursor-pointer w-5 h-5 ${sortOrder === 'asc' && 'text-[#ff2c2c]'}  ${sortOrder === 'desc' && 'text-[#4BB543]'}`} />
                                     <p className="font-montserrat text-[12px] font-bold leading-normal text-[#000000]">Ad və Soyad</p>
                                 </div>
                             </th>
-                            <th className="font-montserrat text-[12px] font-bold leading-normal text-[#000000]">Application Source</th>
-                            <th className="font-montserrat text-[12px] font-bold leading-normal text-[#000000]">Training Name</th>
-                            <th className="font-montserrat text-[12px] font-bold leading-normal text-[#000000]">Mobil nömrə</th>
+                            <th className="font-montserrat text-[12px] py-2 font-bold leading-normal text-[#000000]">Application Source</th>
+                            <th className="font-montserrat text-[12px] py-2  font-bold leading-normal text-[#000000]">Training Name</th>
+                            <th className="font-montserrat text-[12px]  py-2 px-1 font-bold leading-normal text-[#000000]">Mobil nömrə</th>
                             <th  >
                                 <div className="flex gap-1">
                                     <HiOutlineChevronUpDown onClick={handleDateSort} className={`cursor-pointer w-5 h-5 ${dateSortOrder === 'asc' && 'text-[#ff2c2c]'}  ${dateSortOrder === 'desc' && 'text-[#4BB543]'}`} />
                                     <p className="font-montserrat text-[12px] font-bold leading-normal text-[#000000]">Tarix</p>
                                 </div>
                             </th>
-                            <th>
+                            <th className='py-2 '>
                                 <div className="ml-[7px] mt-2 w-[24px] h-[24px]">
                                     <MdOutlineRefresh className="w-[24px] h-[24px]" />
                                 </div>
                             </th>
                         </tr>
-
                     </thead>
                     <tbody >
                         {currentData.map(item => (
                             <tr key={item.id} className="border-b  even:bg-[#fafafa] odd:bg-white">
                                 <>
-                                 <td className="p-2 text-sm text-gray-700">{item.id}</td>
+                                    <td className="p-3  text-sm text-gray-700">{item.id}</td>
                                     <td className="border-none text-center px-4 py-2 font-montserrat text-[14px] font-medium leading-normal text-[#000000]" >
                                         <input type="checkbox" className="appearance-none w-4 h-4 border rounded-[2px] checked:bg-green-500 checked:border-transparent checked:before:text-[10px] focus:outline-none cursor-pointer relative checked:before:content-['✔'] checked:before:absolute checked:before:text-[#fafafa] checked:before:font-normal checked:before:left-1/2 checked:before:top-1/2 checked:before:transform checked:before:-translate-x-1/2 checked:before:-translate-y-1/2" />
                                     </td>
-                                    <td className="font-montserrat text-[14px] font-medium leading-normal text-[#000000]">{item.name}</td>
-                                    <td className="font-montserrat text-[14px] font-medium leading-normal text-[#000000]">{item.applicationSource}</td>
-                                    <td className="font-montserrat text-[14px] font-medium leading-normal text-[#000000]">{item.trainingName}</td>
-                                    <td className="font-montserrat text-[14px] font-medium leading-normal text-[#000000]">{item.phone}</td>
-                                    <td className="font-montserrat text-[14px] font-medium leading-normal text-[#000000]">{item.date}</td>
-                                    <td className="text-center">
+                                    <td className="font-montserrat py-3 text-[14px] font-medium leading-normal text-[#000000]">{item.name}</td>
+                                    <td className="font-montserrat  py-3  text-[14px] font-medium leading-normal text-[#000000]">{item.applicationSource}</td>
+                                    <td className="font-montserrat py-3  text-[14px] font-medium leading-normal text-[#000000]">{item.trainingName}</td>
+                                    <td className="font-montserrat py-3  text-[14px] font-medium leading-normal text-[#000000]">{item.phone}</td>
+                                    <td className="font-montserrat  py-3  text-[14px] font-medium leading-normal text-[#000000]">{item.date}</td>
+                                    <td className=" py-3  text-center">
                                         <div className="flex gap-3">
                                             <FiEdit className="cursor-pointer w-5 h-5" onClick={() => handleEditClick(item)} />
                                             <GoTrash className="cursor-pointer w-5 h-5" onClick={() => handleDeleteClick(item.id)} />
