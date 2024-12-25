5649c1c979bebe53727b8b55b2edfd0caf120794
Khalida <xalide.haqverdiyeva@gmail.com>
2024-12-25 11:39:25 +0400

    feat: clickable top section

--- src/pages/lead-details-page/components/Table Info Top/index.tsx
+++ src/pages/lead-details-page/components/Table Info Top/index.tsx
@@ -22,34 +22,39 @@ const TableInfoTop = () => {
   return (
     <div className="grid grid-cols-6 text-center">
       {numbers.map((number, index) => (
-        <div
-          key={`number-${index}`}
-          className={`flex items-center justify-center text-[24px] font-[500] cursor-pointer transition-all duration-300 ease-in-out ${
-            index === 0 ? "rounded-tl-lg" : ""
-          } ${index === numbers.length - 1 ? "rounded-tr-lg" : ""} ${
-            selectedIndices.includes(index)
-              ? "bg-[#5D7988] text-white"
-              : "bg-[#F4F7FD] text-black"
-          }`}
-          onClick={() => handleClick(index)}
-          style={{ padding: "16px 0" }}
-        >
-          {number}
+        <div className="flex" key={`number-wrapper-${index}`}>
+          <div
+            key={`number-${index}`}
+            className={`flex py-[14px] w-[100%] px-[16px] items-center text-[24px] font-[500] cursor-pointer transition-all duration-300 ease-in-out ${
+              index === 0 ? "rounded-tl-[12px]" : ""
+            } ${index === numbers.length - 1 ? "rounded-tr-[12px]" : ""} ${
+              selectedIndices.includes(index)
+                ? "bg-[#5D7988] text-white"
+                : "bg-[#F4F7FD] text-black"
+            }`}
+            onClick={() => handleClick(index)}
+          >
+            {number}
+          </div>
+          {index < numbers.length - 1 && (
+            <div className="flex items-center h-full bg-[#F4F7FD]">
+              <div className="h-[30px] w-[2px] bg-[#5D7988] rounded-full"></div>
+            </div>
+          )}
         </div>
       ))}
 
       {texts.map((text, index) => (
         <div
           key={`text-${index}`}
-          className={`bg-[#5D7988] text-[12px] text-white flex items-center justify-center transition-opacity duration-300 ease-in-out cursor-pointer ${
-            index === 0 ? "rounded-bl-lg" : ""
-          } ${index === texts.length - 1 ? "rounded-br-lg" : ""} ${
+          className={`bg-[#5D7988] py-[14px] px-[16px] text-[12px] text-white flex items-center transition-opacity duration-300 ease-in-out cursor-pointer ${
+            index === 0 ? "rounded-bl-[12px]" : ""
+          } ${index === texts.length - 1 ? "rounded-br-[12px]" : ""} ${
             selectedIndices.includes(index + numbers.length)
               ? "bg-[#F4F7FD] text-black"
               : "bg-[#5D7988] text-white"
           }`}
           onClick={() => handleClick(index + numbers.length)}
-          style={{ padding: "16px 0" }}
         >
           {text}
         </div>
